d9a37b218d2c393de0a0868a39590730597c9128
isuru <isuru@ftservices.net>
2021-08-25 02:11:40 +0530

    created and seperated module and services

---
+++ TasksWebAPI/Controllers/TodosController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TasksWebAPI.Models;
+using TasksWebAPI.Services;
+
+namespace TasksWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodosController : ControllerBase
+    {
+        private TodoService _TodoService;
+
+        public TodosController()
+        {
+            _TodoService = new TodoService(); 
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetTodos(int? id)
+        {
+            var myTodos = _TodoService.AllTodos();
+            if (id is null) return  Ok(myTodos);
+            myTodos = myTodos.Where (t=> t.id==id).ToList();
+            return Ok(myTodos);
+        }
+
+       
+    }
+}
---
+++ TasksWebAPI/Services/TodoService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TasksWebAPI.Models;
+
+namespace TasksWebAPI.Services
+{
+    public class TodoService
+    {
+        public  List<Todo> AllTodos()
+        {
+            var Todos = new List<Todo>();
+            var Todo1 = new Todo
+            {
+                id = 1,
+                Title = "Get New Books",
+                Description = "Get Some Text Books",
+                Created = DateTime.Now,
+                Due = DateTime.Now.AddDays(5),
+                Status = TodoStatus.New
+            };
+            Todos.Add(Todo1);
+            var Todo2 = new Todo
+            {
+                id = 2,
+                Title = "Get Bag",
+                Description = "Get new bag",
+                Created = DateTime.Now,
+                Due = DateTime.Now.AddDays(5),
+                Status = TodoStatus.Completed
+            };
+            Todos.Add(Todo2);
+            var Todo3 = new Todo
+            {
+                id = 3,
+                Title = "Get shoes",
+                Description = "Get new shoes",
+                Created = DateTime.Now,
+                Due = DateTime.Now.AddDays(5),
+                Status = TodoStatus.New
+            };
+            Todos.Add(Todo3);
+            return Todos;
+
+        }
+    }
+}
